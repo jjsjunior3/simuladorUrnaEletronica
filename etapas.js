91f540b40bbcc80dea9fc6ae387f2ce8f1fa69ef
Junior Santos <junior.ead@hotmail.com>
2022-07-30 10:31:49 -0300

    finalizando o script da urna

---
+++ etapas.js
@@ -0,0 +1,60 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        condidatos: [
+            {
+                numero: '12345',
+                nome: 'GOKU',
+                partido: 'DBZ',
+                fotos:[
+                    {url:'goku.png', legenda:'Vereador'}
+                ]
+            }, 
+            {
+                numero: '67890',
+                nome: 'SEIYA',
+                partido: 'CDZ',
+                fotos:[
+                    {url:'seiya.png', legenda:'Vereador'}
+                ]
+            },
+            {
+                numero: '77777',
+                nome: 'MINIONS',
+                partido: 'PIXAS',
+                fotos:[
+                    {url:'minions.png', legenda:'Vereador'}
+                ]
+            },
+
+        ]
+    },
+    {
+        titulo:'PRESIDENTE',
+        numeros: 2,
+        condidatos: [
+            {
+                numero: '17',
+                nome:'BOLSONARO',
+                partido: 'PL',
+                vice: 'Mourão',
+                fotos:[
+                    {url:'bolsonaro17.png', legenda: 'Presidente'},
+                    {url:'mourao.png', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '113',
+                nome:'lula',
+                partido: 'PT',
+                vice: 'Alckmin',
+                fotos:[
+                    {url:'lula 13.png', legenda: 'Presidente'},
+                    {url:'Alckmin13-2.png', legenda: 'Vice-Presidente', small: true}
+                ]
+            }
+
+        ]
+    }
+];
